d04855ae4b3d151b973797cc2048ecddcb0ca094
marina <marinafront@hotmail.fr>
2018-05-30 13:03:03 +0200

    essai de récuperation des articles

--- index.php
+++ index.php
@@ -16,3 +16,11 @@ echo json_encode(['error' => false, 'payload' => $resultats]);
 return true;
 
 
+
+
+$images = [
+	["id"=>1, "id_article"=>1, "alt"=> "captures d'écran de l'appli web", "image"=>"../images/imprimEcranEx.jpg"],
+	["id"=>2, "id_article"=>1, "alt"=> "architecture du systeme", "image"=>"../images/archi-materiel.png"],
+	["id"=>3, "id_article"=>1, "alt"=> "diagrame de cas d'utilisation", "image"=>"../images/diagramCasUtilisationexemples.jpg"],
+	["id"=>4, "id_article"=>2, "alt"=> "maquette de la page d'accueil", "image"=>"../images/site randonnée-2.png"],	
+];
--- realisation-article.php
+++ realisation-article.php
@@ -1,1 +1,19 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+$articleRea=[
+	["id"=>1, "titre"=> "Home Control",
+	 "contenu"=>"Ce projet a été réalisé dans le cadre de la formation Développement et Administration System d'Information Expert 2018 (Greta du Var). C'est un projet que nous avons réalisé en 2 mois en groupe : 12 personnes. les enjeux étant d'utiliser nos aquis de la formation.
+	L'objectif du projet était de développer une application Android et une application web permettant de piloter différents équipements d'un domicile (luminaires, stores et chauffages). Ceci de manière direct ou au travers de scénario exécutable.  Ainsi que la suveillance du domicile grâce à des cameras pilotables elles aussi.
+	La demande comportait aussi une gestion des rôles des utilisateurs qui n'ont pas les mêmes droits d'acces à l'application."],
+	["id"=>2, "titre"=> "Randonnée",
+	 "contenu"=>"1 présentation
+					1.1 Contexte
+	Lorsque j'ai commencé mon apprentissage du développement j'ai suivi des cours sur internet mais il me falait un support sur lequel m'entrainer j'ai donc voulu créer un  site internet. le theme je l'ai choisi par affinité. j'ai essayé de le faire le plus complet possible pour y inclure toutes mes connaissances.
+	Ce site n'est pas en ligne, il n'est pas teminé.
+"],
+];
+
+echo json_encode(['error' => false, 'payload' => $articleRea]);
+return true;
