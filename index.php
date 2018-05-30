6eb15a900571f37a8724c8b59e6e43fea1db8f7a
marina <marinafront@hotmail.fr>
2018-05-30 22:13:12 +0200

    l'api renvoie bien l'article demandé grâce à l'id passé en get

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
 
-$resultats = [
-	["id"=>1, "titre"=> "Projet tutoré en groupe", "image"=>"domotique"],
-	["id"=>2, "titre"=> "Un site de randonnée complet", "image"=>"randonnee"],
-	["id"=>3, "titre"=> "Une landing page one page", "image"=>"ptidej"],
-	["id"=>4, "titre"=> "Un TP React", "image"=>"pendu"],
-	["id"=>5, "titre"=> "Un site de cuisine", "image"=>"cuisiner"],
+/*$resultats = [
+	["id"=>1, "titreRea"=> "Projet tutoré en groupe", "image"=>"domotique"],
+	["id"=>2, "titreRea"=> "Un site de randonnée complet", "image"=>"randonnee"],
+	["id"=>3, "titreRea"=> "Une landing page one page", "image"=>"ptidej"],
+	["id"=>4, "titreRea"=> "Un TP React", "image"=>"pendu"],
+	["id"=>5, "titreRea"=> "Un site de cuisine", "image"=>"cuisiner"],
 	
-];
+];*/
 
 echo json_encode(['error' => false, 'payload' => $resultats]);
 return true;
@@ -19,8 +19,8 @@ return true;
 
 
 $images = [
-	["id"=>1, "id_article"=>1, "alt"=> "captures d'écran de l'appli web", "image"=>"../images/imprimEcranEx.jpg"],
-	["id"=>2, "id_article"=>1, "alt"=> "architecture du systeme", "image"=>"../images/archi-materiel.png"],
-	["id"=>3, "id_article"=>1, "alt"=> "diagrame de cas d'utilisation", "image"=>"../images/diagramCasUtilisationexemples.jpg"],
-	["id"=>4, "id_article"=>2, "alt"=> "maquette de la page d'accueil", "image"=>"../images/site randonnée-2.png"],	
+	["id"=>1, "id_article"=>1, "alt"=> "captures d'écran de l'appli web", "image"=>"images/imprimEcranEx.jpg"],
+	["id"=>2, "id_article"=>1, "alt"=> "architecture du systeme", "image"=>"images/archi-materiel.png"],
+	["id"=>3, "id_article"=>1, "alt"=> "diagrame de cas d'utilisation", "image"=>"images/diagramCasUtilisationexemples.jpg"],
+	["id"=>4, "id_article"=>2, "alt"=> "maquette de la page d'accueil", "image"=>"images/site randonnée-2.png"],	
 ];
--- realisation-article.php
+++ realisation-article.php
@@ -3,16 +3,19 @@ header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
 $articleRea=[
-	["id"=>1, "titre"=> "Home Control",
+	["id"=>1, "image"=>"domotique", "titre"=> "Home Control",
 	 "contenu"=>"Ce projet a été réalisé dans le cadre de la formation Développement et Administration System d'Information Expert 2018 (Greta du Var). C'est un projet que nous avons réalisé en 2 mois en groupe : 12 personnes. les enjeux étant d'utiliser nos aquis de la formation.
 	L'objectif du projet était de développer une application Android et une application web permettant de piloter différents équipements d'un domicile (luminaires, stores et chauffages). Ceci de manière direct ou au travers de scénario exécutable.  Ainsi que la suveillance du domicile grâce à des cameras pilotables elles aussi.
 	La demande comportait aussi une gestion des rôles des utilisateurs qui n'ont pas les mêmes droits d'acces à l'application."],
-	["id"=>2, "titre"=> "Randonnée",
-	 "contenu"=>"1 présentation
-					1.1 Contexte
+	["id"=>2, "titre"=> "Un site de randonnée","image"=>"randonnee",
+	 "contenu"=>"<h2>1 présentation</h2>
+					<h3>1.1 Contexte</h3>
 	Lorsque j'ai commencé mon apprentissage du développement j'ai suivi des cours sur internet mais il me falait un support sur lequel m'entrainer j'ai donc voulu créer un  site internet. le theme je l'ai choisi par affinité. j'ai essayé de le faire le plus complet possible pour y inclure toutes mes connaissances.
 	Ce site n'est pas en ligne, il n'est pas teminé.
-"],
+	"],
+	["id"=>3, "titre"=> "Ptitdej.fr", "image"=>"ptidej", "contenu"=>""],
+	["id"=>4, "titre"=> "Un TP React", "image"=>"pendu","contenu"=>""],
+	["id"=>5, "titre"=> "Quoicuisiner.fr", "image"=>"cuisiner", "contenu"=>""],
 ];
 
 
@@ -22,7 +25,13 @@ if(!empty($_GET['id'])){
             continue;
         }
        
-        echo json_encode(['error' => false, 'payload' => [$article]]);
+        echo json_encode(['error' => false, 'payload' => $article]);
         return true;
     }
+    echo json_encode(['error' => true, 'payload' => "pas d'article"]);
+        return true;
 }
+
+
+ echo json_encode(['error' => false, 'payload' => $articleRea]);
+        return true;
