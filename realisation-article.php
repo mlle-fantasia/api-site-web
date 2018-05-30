2fbb67cfe7c8cf8d93ab9829101e5f67c1379d24
marina <marinafront@hotmail.fr>
2018-05-30 16:40:41 +0200

    décommenter les fonctions header

--- realisation-article.php
+++ realisation-article.php
@@ -1,6 +1,6 @@
 <?php
-/*header('Access-Control-Allow-Origin: *');
-header('Content-type: application/json');*/
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
 
 $articleRea=[
 	["id"=>1, "titre"=> "Home Control",
